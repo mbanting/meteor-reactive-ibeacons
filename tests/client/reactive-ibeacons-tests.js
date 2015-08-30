d40eeea89638fe23b1f4874dca9fa1b5c6706ade
Marty Banting <mbanting@yahoo.com>
2015-08-29 18:33:53 -0700

    Consolidated and renamed test suites

--- tests/client/reactive-ibeacons-tests.js
+++ tests/client/reactive-ibeacons-tests.js
@@ -7,21 +7,15 @@ Tinytest.add('ReactiveBeaconRegion exists', function (test) {
 });
 
 /**
- * Test to ensure ReactiveBeaconRegion accepts a valid beaconRegion
+ * Test to ensure ReactiveBeaconRegion accepts a valid beaconRegion and disable flags
  */
-Tinytest.add('beaconRegion is valid', function (test) {
+Tinytest.add('new ReactiveBeaconRegion()', function (test) {
     test.throws(function(){new ReactiveBeaconRegion()});
     test.throws(function(){new ReactiveBeaconRegion({identifier:1, uuid:"123"})});
     test.throws(function(){new ReactiveBeaconRegion({identifier:"123", uuid:1})});
     test.throws(function(){new ReactiveBeaconRegion({identifier:"123", uuid:"123", major:1, minor:"123"})});
     test.throws(function(){new ReactiveBeaconRegion({identifier:"123", uuid:"123", major:"123", minor:1})});
     test.instanceOf(new ReactiveBeaconRegion({identifier:"123", uuid:"123", major:"123", minor:"123"}), ReactiveBeaconRegion);
-});
-
-/**
- * Test to ensure ReactiveBeaconRegion accepts a valid disable flags
- */
-Tinytest.add('disableMonitoring and disableRanging are valid', function (test) {
     test.throws(function(){new ReactiveBeaconRegion({identifier:"123", uuid:"123", major:"123", minor:"123"}, 1, true)});
     test.throws(function(){new ReactiveBeaconRegion({identifier:"123", uuid:"123", major:"123", minor:"123"}, true, 1)});
     test.instanceOf(new ReactiveBeaconRegion({identifier:"123", uuid:"123", major:"123", minor:"123"}, true, true), ReactiveBeaconRegion);
@@ -34,27 +28,21 @@ Tinytest.add('disableMonitoring and disableRanging are valid', function (test) {
 // Need spy support or do it via the locationManager stub
 
 /**
- * Test areBeaconsUpdated detecting same collection of beacon information
+ * Test areBeaconsUpdated detecting changes in beacon information
  */
-Tinytest.add('same beacon information is detected', function (test) {
+Tinytest.add('ReactiveBeaconRegion.areBeaconsUpdated()', function (test) {
     var reactiveBeaconRegion = new ReactiveBeaconRegion({identifier:"123", uuid:"123", major:"123", minor:"123"});
+
     var existingBeacons = [
         {"minor": 13911, "rssi": -65, "major": 22728, "proximity": "ProximityImmediate", "accuracy": 0.12, "uuid": "F7826DA6-4FA2-4E98-8024-BC5B71E0893E"},
         {"minor": 13912, "rssi": -66, "major": 22728, "proximity": "ProximityNear", "accuracy": 0.11, "uuid": "F7826DA6-4FA2-4E98-8024-BC5B71E0893E"}
     ];
+
+    // first test with same beacons
     reactiveBeaconRegion.beaconRegion.beacons = existingBeacons;
     test.isFalse(reactiveBeaconRegion.areBeaconsUpdated(existingBeacons), "Expected beacon comparison to detect same beacons");
-});
 
-/**
- * Test areBeaconsUpdated detecting different collection of beacon information
- */
-Tinytest.add('different beacon information is detected', function (test) {
-    var reactiveBeaconRegion = new ReactiveBeaconRegion({identifier:"123", uuid:"123", major:"123", minor:"123"});
-    var existingBeacons = [
-        {"minor": 13911, "rssi": -65, "major": 22728, "proximity": "ProximityImmediate", "accuracy": 0.12, "uuid": "F7826DA6-4FA2-4E98-8024-BC5B71E0893E"},
-        {"minor": 13912, "rssi": -66, "major": 22728, "proximity": "ProximityNear", "accuracy": 0.11, "uuid": "F7826DA6-4FA2-4E98-8024-BC5B71E0893E"}
-    ];
+    // now test with change to existing beacons and with an additional beacon
     var differentBeacons = [
         {"minor": 13911, "rssi": -65, "major": 22728, "proximity": "ProximityImmediate", "accuracy": 0.12, "uuid": "F7826DA6-4FA2-4E98-8024-BC5B71E0893E"},
         {"minor": 13912, "rssi": -66, "major": 22728, "proximity": "ProximityFar" /* changed */,
@@ -67,32 +55,14 @@ Tinytest.add('different beacon information is detected', function (test) {
     ];
     reactiveBeaconRegion.beaconRegion.beacons = existingBeacons;
     test.isTrue(reactiveBeaconRegion.areBeaconsUpdated(differentBeacons), "Expected beacon comparison to detect different beacons");
-    test.isTrue(reactiveBeaconRegion.areBeaconsUpdated(additionalBeacons), "Expected beacon comparison to detect different beacons");
+    test.isTrue(reactiveBeaconRegion.areBeaconsUpdated(additionalBeacons), "Expected beacon comparison to the new beacon");
 });
 
-
-/**
- * Test areBeaconsUpdated detecting an updated collection with an additional beacon added
- */
-Tinytest.add('different beacon information with new beacon is detected', function (test) {
-    var reactiveBeaconRegion = new ReactiveBeaconRegion({identifier:"123", uuid:"123", major:"123", minor:"123"});
-    var existingBeacons = [
-        {"minor": 13911, "rssi": -65, "major": 22728, "proximity": "ProximityImmediate", "accuracy": 0.12, "uuid": "F7826DA6-4FA2-4E98-8024-BC5B71E0893E"},
-        {"minor": 13912, "rssi": -66, "major": 22728, "proximity": "ProximityNear", "accuracy": 0.11, "uuid": "F7826DA6-4FA2-4E98-8024-BC5B71E0893E"}
-    ];
-    var newBeacons = [
-        {"minor": 13911, "rssi": -65, "major": 22728, "proximity": "ProximityImmediate", "accuracy": 0.12, "uuid": "F7826DA6-4FA2-4E98-8024-BC5B71E0893E"},
-        {"minor": 13912, "rssi": -66, "major": 22728, "proximity": "ProximityNear", "accuracy": 0.11, "uuid": "F7826DA6-4FA2-4E98-8024-BC5B71E0893E"},
-        {"minor": 13912, "rssi": -66, "major": 22728, "proximity": "ProximityNear", "accuracy": 0.11, "uuid": "F7826DA6-4FA2-4E98-8024-BC5B71E0893E"}
-    ];
-    reactiveBeaconRegion.beaconRegion.beacons = existingBeacons;
-    test.isTrue(reactiveBeaconRegion.areBeaconsUpdated(newBeacons), "Expected beacon comparison to detect different beacons");
-});
 
 /**
  * Test reactivity on updated beacon monitoring
  */
-Tinytest.add('reactive computation updates when ReactiveBeaconRegion monitoring updates', function (test) {
+Tinytest.add('ReactiveBeaconRegion.delegate.didDetermineStateForRegion()', function (test) {
     var inRegion = false;
     var reactiveBeaconRegion = new ReactiveBeaconRegion({identifier:"123", uuid:"123", major:"123", minor:"123"});
     var computationRanAgain = false;
@@ -140,7 +110,7 @@ Tinytest.add('reactive computation updates when ReactiveBeaconRegion monitoring 
 /**
  * Test reactivity on updated beacon ranging
  */
-Tinytest.add('reactive computation updates when ReactiveBeaconRegion ranging updates', function (test) {
+Tinytest.add('ReactiveBeaconRegion.delegate.didRangeBeaconsInRegion()', function (test) {
     var computationRanAgain = false;
     var reactiveBeaconRegion = new ReactiveBeaconRegion({identifier:"123", uuid:"123", major:"123", minor:"123"});
     Tracker.autorun(function(computation) {
