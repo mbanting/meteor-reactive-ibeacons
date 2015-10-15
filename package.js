2fc68e46cb862cb89130cbb826a9fd5e39ae21e4
Matthew Lowe <matt@literateprogrammer.com>
2015-10-15 15:40:39 -0500

    add basic hosting

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'mbanting:reactive-ibeacons',
   summary: "Turns iBeacons into reactive data sources in your Meteor Cordova apps.",
-  version: '1.0.3',
+  version: '1.1.0',
   git: "https://github.com/mbanting/meteor-reactive-ibeacons"
 });
 
@@ -18,6 +18,7 @@ Package.onUse(function(api) {
     api.use(['check', 'tracker', 'underscore']);
     api.addFiles('lib/reactive-ibeacons.js', ["web.cordova"]);
     api.export('ReactiveBeaconRegion', ['web.cordova']);
+    api.export('ReactiveBeaconAdvertiser', ['web.cordova']);
 
 });
 
@@ -27,4 +28,5 @@ Package.onTest(function(api) {
     api.addFiles(['tests/client/stubs.js', 'lib/reactive-ibeacons.js'], ["client"]); // tests can run on client, make files available
     api.addFiles('tests/client/reactive-ibeacons-tests.js', 'client');
     api.export('ReactiveBeaconRegion', 'client');
+    api.export('ReactiveBeaconAdvertiser', 'client');
 });
